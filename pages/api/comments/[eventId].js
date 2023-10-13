34011f8b7e9c7c5253065c24ad0ee0fddfe32b30
kthawani <kthawani@entera.ai>
2023-10-13 16:59:59 +0530

    add comment api route

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,38 @@
+function handler(req, res) {
+  const eventId = req.query.eventId;
+
+  if (req.method === "POST") {
+    const { name, email, text } = req.body;
+
+    if (
+      email.includes("@") ||
+      !name ||
+      name.trim() == "" ||
+      !text ||
+      text.trim() == ""
+    ) {
+      res.status(422).json({ message: "Invalid Input" });
+      return;
+    }
+
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+    console.log(newComment);
+    res.status(201).json({ message: "Added Comment", comment: newComment });
+  }
+
+  if (req.method === "GET") {
+    const dummyList = [
+      { id: "c1", name: "Max", comment: "A First Comment" },
+      { id: "c2", name: "Max", comment: "A Second Comment" },
+    ];
+
+    res.status(200).json({ comments: dummyList });
+  }
+}
+
+export default handler;
