40dd1e7bf36b258207503ffab63f6b959c99577d
kthawani <kthawani@entera.ai>
2023-10-17 12:35:41 +0530

    context state added

--- store/notification-context.js
+++ store/notification-context.js
@@ -2,13 +2,29 @@ import { createContext, useState } from "react";
 
 const notificationContext = createContext({
   notification: null,
-  showNotification: function () {},
+  showNotification: function (notificationData) {},
   hideNotification: function () {},
 });
 
 export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
-    <notificationContext.Provider>
+    <notificationContext.Provider value={context}>
       {props.chiildren}
     </notificationContext.Provider>
   );
